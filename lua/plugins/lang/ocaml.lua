f08738fd442394e790c2ff9159076ee85f256f34
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-06-23 19:35:18 -0700

    Improved ocamllsp config

--- lua/plugins/lang/ocaml.lua
+++ lua/plugins/lang/ocaml.lua
@@ -1,18 +1,34 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ocamllsp = {},
-      },
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "ocaml" })
       end
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ocamllsp = {
+          get_language_id = function(_, ftype)
+            return ftype
+          end,
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "*.opam",
+              "esy.json",
+              "package.json",
+              ".git",
+              "dune-project",
+              "dune-workspace",
+              "*.ml"
+            )(fname)
+          end,
+          on_attach = on_attach,
+        },
+      },
+    },
   },
 }
