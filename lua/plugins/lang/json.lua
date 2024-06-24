ff13f29218f2dd38c6410017edef6a48e76c9d02
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-06-23 20:24:17 -0700

    Added more keybinds

--- lua/plugins/lang/json.lua
+++ lua/plugins/lang/json.lua
@@ -3,8 +3,18 @@ return {
     "VPavliashvili/json-nvim", -- requires jq (installed with mason later)
     ft = "json",
     keys = {
-      { "<localleader>f", "<cmd>JsonFormatFile<cr>", ft = "json", desc = "format json with jq" },
-      { "<localleader>m", "<cmd>JsonMinifyFile<cr>", ft = "json", desc = "minify json with jq" },
+      { "<localleader>e", "<cmd>JsonEscapeFile<cr>", ft = "json", desc = "Escape JSON in entire file" },
+      { "<localleader>E", "<cmd>JsonEscapeSelection<cr>", ft = "json", desc = "Escape JSON in selected text" },
+      { "<localleader>f", "<cmd>JsonFormatFile<cr>", ft = "json", desc = "Format JSON in entire file" },
+      { "<localleader>F", "<cmd>JsonFormatSelection<cr>", ft = "json", desc = "Format JSON in selected text" },
+      { "<localleader>t", "<cmd>JsonFormatToken<cr>", ft = "json", desc = "Format JSON token under cursor" },
+      { "<localleader>c", "<cmd>JsonKeysToCamelCase<cr>", ft = "json", desc = "Convert JSON keys to camelCase" },
+      { "<localleader>p", "<cmd>JsonKeysToPascalCase<cr>", ft = "json", desc = "Convert JSON keys to PascalCase" },
+      { "<localleader>s", "<cmd>JsonKeysToSnakeCase<cr>", ft = "json", desc = "Convert JSON keys to snake_case" },
+      { "<localleader>m", "<cmd>JsonMinifyFile<cr>", ft = "json", desc = "Minify JSON in entire file" },
+      { "<localleader>M", "<cmd>JsonMinifySelection<cr>", ft = "json", desc = "Minify JSON in selected text" },
+      { "<localleader>u", "<cmd>JsonUnescapeFile<cr>", ft = "json", desc = "Unescape JSON in entire file" },
+      { "<localleader>U", "<cmd>JsonUnescapeSelection<cr>", ft = "json", desc = "Unescape JSON in selected text" },
     },
   },
   {
