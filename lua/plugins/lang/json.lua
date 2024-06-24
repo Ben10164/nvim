c1b50db652241de7a026816d34e63a476674ac1c
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-06-23 19:33:52 -0700

    added schemastore directly

--- lua/plugins/lang/json.lua
+++ lua/plugins/lang/json.lua
@@ -19,6 +19,7 @@ return {
           end,
           settings = {
             json = {
+              schemas = require("schemastore").json.schemas(),
               format = {
                 enable = true,
               },
