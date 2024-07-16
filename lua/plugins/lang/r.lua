b0cfaee6f6c53eb79e5359b4f7c634a5121f8f11
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-07-16 09:20:46 -0700

    which-key v3

--- lua/plugins/lang/r.lua
+++ lua/plugins/lang/r.lua
@@ -22,22 +22,20 @@ return {
           })
 
           -- Workaround from https://github.com/folke/which-key.nvim/issues/514#issuecomment-1987286901
-          wk.register({
-            ["<localleader>"] = {
-              a = { name = "+(a)ll", ["🚫"] = "which_key_ignore" },
-              b = { name = "+(b)etween marks", ["🚫"] = "which_key_ignore" },
-              c = { name = "+(c)hunks", ["🚫"] = "which_key_ignore" },
-              f = { name = "+(f)unctions", ["🚫"] = "which_key_ignore" },
-              g = { name = "+(g)oto", ["🚫"] = "which_key_ignore" },
-              i = { name = "+(i)nstall", ["🚫"] = "which_key_ignore" },
-              k = { name = "+(k)nit", ["🚫"] = "which_key_ignore" },
-              p = { name = "+(p)aragraph", ["🚫"] = "which_key_ignore" },
-              q = { name = "+(q)uarto", ["🚫"] = "which_key_ignore" },
-              r = { name = "+(r) general", ["🚫"] = "which_key_ignore" },
-              s = { name = "+(s)plit or (s)end", ["🚫"] = "which_key_ignore" },
-              t = { name = "+(t)erminal", ["🚫"] = "which_key_ignore" },
-              v = { name = "+(v)iew", ["🚫"] = "which_key_ignore" },
-            },
+          wk.add({
+            { "<localleader>a", group = "(a)ll" },
+            { "<localleader>b", group = "(b)etween marks" },
+            { "<localleader>c", group = "(c)hunks" },
+            { "<localleader>f", group = "(f)unctions" },
+            { "<localleader>g", group = "(g)oto" },
+            { "<localleader>i", group = "(i)nstall" },
+            { "<localleader>k", group = "(k)nit" },
+            { "<localleader>p", group = "(p)aragraph" },
+            { "<localleader>q", group = "(q)uarto" },
+            { "<localleader>r", group = "(r) general" },
+            { "<localleader>s", group = "(s)plit or (s)end" },
+            { "<localleader>t", group = "(t)erminal" },
+            { "<localleader>v", group = "(v)iew" },
           })
         end,
       },
